98accdbaf5adfb5cfd641920d23e8f50e38ce338
jhaworth21 <83611597+jhaworth21@users.noreply.github.com>
2024-04-29 14:03:10 +0100

    fixed issue with schooling behaviour in blue tangs - need to add weightings back in and goal generation

--- Assets/Scripts/School.cs
+++ Assets/Scripts/School.cs
@@ -5,118 +5,234 @@ using UnityEngine;
 public class School : MonoBehaviour
 {
     float speed;
-    bool turning;
+    bool turning = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(SchoolManager.SM.minSpeed, SchoolManager.SM.maxSpeed);
 
+        speed = Random.Range(SchoolManager.SM.minSpeed, SchoolManager.SM.maxSpeed);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Bounds b = new Bounds(SchoolManager.SM.transform.position, SchoolManager.SM.schoolLimit * 2);
+
+        Bounds b = new Bounds(SchoolManager.SM.transform.position, SchoolManager.SM.schoolLimits * 2.0f);
 
         if (!b.Contains(transform.position))
         {
+
             turning = true;
         }
         else
         {
+
             turning = false;
         }
 
         if (turning)
         {
+
             Vector3 direction = SchoolManager.SM.transform.position - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation,
-                                                  Quaternion.LookRotation(direction),
-                                                  SchoolManager.SM.rotationSpeed * Time.deltaTime);
-            Debug.Log("Transform Rotation = " +  transform.rotation);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(direction),
+                speed * Time.deltaTime);
         }
         else
         {
+
+
             if (Random.Range(0, 100) < 10)
             {
+
                 speed = Random.Range(SchoolManager.SM.minSpeed, SchoolManager.SM.maxSpeed);
+            }
+
+
+            if (Random.Range(0, 100) < 10)
+            {
                 ApplyRules();
             }
         }
-        gameObject.transform.Translate(0, 0, speed * Time.deltaTime);
+
+        this.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
     }
 
-    /// <summary>
-    /// Applies the boids model ruleset to the movement of the gameObject
-    /// </summary>
-    void ApplyRules()
+    private void ApplyRules()
     {
-        //gets all of the fish in the school
-        GameObject[] allFish;
-        allFish = SchoolManager.SM.allFish;
 
-        //initialises the center and avoidance vectors
-        Vector3 vcenter = Vector3.zero;
-        Vector3 vavoid = Vector3.zero;
+        GameObject[] gos;
+        gos = SchoolManager.SM.allFish;
 
-        //sets a base value for the group speed and size
+        Vector3 vCentre = Vector3.zero;
+        Vector3 vAvoid = Vector3.zero;
+
         float gSpeed = 0.01f;
+        float mDistance;
         int groupSize = 0;
 
-        //creates the varaible representing neighbour distance
-        float nDistance;
-
-        //loops through all fish in the school
-        foreach (GameObject fish in allFish)
+        foreach (GameObject go in gos)
         {
-            //if not current fish
-            if (fish != gameObject)
+
+            if (go != this.gameObject)
             {
-                //gets the distance between current fish and currently fish at position in the loop
-                nDistance = Vector3.Distance(fish.transform.position, gameObject.transform.position);
-                //comparison against neighbour distance limit
-                if (nDistance <= SchoolManager.SM.neighbourDistance)
+
+                mDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (mDistance <= SchoolManager.SM.neighbourDistance)
                 {
-                    //adds the position to the center vector based on weighting value
-                    vcenter += SchoolManager.SM.cohesionWeighting * fish.transform.position;
+
+                    vCentre += go.transform.position;
                     groupSize++;
 
-                    //if too close
-                    if (nDistance < 1.0f)
+                    if (mDistance < 1.0f)
                     {
-                        //adds the difference to avoidance vector with weighting applied
-                        vavoid = vavoid * SchoolManager.SM.separationWeighting + (gameObject.transform.position - fish.transform.position);
+
+                        vAvoid = vAvoid + (this.transform.position - go.transform.position);
                     }
 
-                    //gets the other schools in the overall swarm
-                    School anotherFlock = fish.GetComponent<School>();
-                    gSpeed = gSpeed + anotherFlock.speed;
+                    School anotherSchool = go.GetComponent<School>();
+                    gSpeed = gSpeed + anotherSchool.speed;
                 }
             }
         }
 
-
-        //if school has any fish
         if (groupSize > 0)
         {
-            //center is the average position
-            vcenter = vcenter / groupSize + (SchoolManager.SM.goalPos - gameObject.transform.position);
-            //average the speed across the school
+
+            vCentre = vCentre / groupSize + (SchoolManager.SM.goalPos - this.transform.position);
             speed = gSpeed / groupSize;
-            //resets speed to max if max exceeded
+
             if (speed > SchoolManager.SM.maxSpeed)
             {
+
                 speed = SchoolManager.SM.maxSpeed;
             }
 
-            //creates the direction vector to control the forward movement of the fish
-            Vector3 direction = (vcenter + vavoid) * SchoolManager.SM.alignmentWeighting - transform.position;
+            Vector3 direction = (vCentre + vAvoid) - transform.position;
             if (direction != Vector3.zero)
-                transform.rotation = Quaternion.Slerp(transform.rotation,
-                                                      Quaternion.LookRotation(direction),
-                                                      SchoolManager.SM.rotationSpeed * Time.deltaTime);
+            {
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    Quaternion.LookRotation(direction),
+                    SchoolManager.SM.rotationSpeed * Time.deltaTime);
+            }
         }
     }
+    //float speed;
+    //bool turning;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    speed = Random.Range(SchoolManager.SM.minSpeed, SchoolManager.SM.maxSpeed);
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Bounds b = new Bounds(SchoolManager.SM.transform.position, SchoolManager.SM.schoolLimit);
+
+    //    if (!b.Contains(transform.position))
+    //    {
+    //        turning = true;
+    //    }
+    //    else
+    //    {
+    //        turning = false;
+    //    }
+
+    //    if (turning)
+    //    {
+    //        Vector3 direction = SchoolManager.SM.transform.position - transform.position;
+    //        transform.rotation = Quaternion.Slerp(transform.rotation,
+    //                                              Quaternion.LookRotation(direction),
+    //                                              SchoolManager.SM.rotationSpeed * Time.deltaTime);
+    //        Debug.Log("Transform Rotation = " +  transform.rotation);
+    //    }
+    //    else
+    //    {
+    //        if (Random.Range(0, 100) < 10)
+    //        {
+    //            speed = Random.Range(SchoolManager.SM.minSpeed, SchoolManager.SM.maxSpeed);
+    //            ApplyRules();
+    //        }
+    //    }
+    //    gameObject.transform.Translate(0, 0, speed * Time.deltaTime);
+    //}
+
+    ///// <summary>
+    ///// Applies the boids model ruleset to the movement of the gameObject
+    ///// </summary>
+    //void ApplyRules()
+    //{
+    //    //gets all of the fish in the school
+    //    GameObject[] allFish;
+    //    allFish = SchoolManager.SM.allFish;
+
+    //    //initialises the center and avoidance vectors
+    //    Vector3 vcenter = Vector3.zero;
+    //    Vector3 vavoid = Vector3.zero;
+
+    //    //sets a base value for the group speed and size
+    //    float gSpeed = 0.01f;
+    //    int groupSize = 0;
+
+    //    //creates the varaible representing neighbour distance
+    //    float nDistance;
+
+    //    //loops through all fish in the school
+    //    foreach (GameObject fish in allFish)
+    //    {
+    //        //if not current fish
+    //        if (fish != gameObject)
+    //        {
+    //            //gets the distance between current fish and currently fish at position in the loop
+    //            nDistance = Vector3.Distance(fish.transform.position, gameObject.transform.position);
+    //            //comparison against neighbour distance limit
+    //            if (nDistance <= SchoolManager.SM.neighbourDistance)
+    //            {
+    //                //adds the position to the center vector based on weighting value
+    //                vcenter += SchoolManager.SM.cohesionWeighting * fish.transform.position;
+    //                groupSize++;
+
+    //                //if too close
+    //                if (nDistance < 1.0f)
+    //                {
+    //                    //adds the difference to avoidance vector with weighting applied
+    //                    vavoid = vavoid * SchoolManager.SM.separationWeighting + (gameObject.transform.position - fish.transform.position);
+    //                }
+
+    //                //gets the other schools in the overall swarm
+    //                School anotherSchool = fish.GetComponent<School>();
+    //                gSpeed = gSpeed + anotherSchool.speed;
+    //            }
+    //        }
+    //    }
+
+
+    //    //if school has any fish
+    //    if (groupSize > 0)
+    //    {
+    //        //center is the average position
+    //        vcenter = vcenter / groupSize + (SchoolManager.SM.goalPos - gameObject.transform.position);
+    //        //average the speed across the school
+    //        speed = gSpeed / groupSize;
+    //        //resets speed to max if max exceeded
+    //        if (speed > SchoolManager.SM.maxSpeed)
+    //        {
+    //            speed = SchoolManager.SM.maxSpeed;
+    //        }
+
+    //        //creates the direction vector to control the forward movement of the fish
+    //        Vector3 direction = (vcenter + vavoid) * SchoolManager.SM.alignmentWeighting - transform.position;
+    //        if (direction != Vector3.zero)
+    //            transform.rotation = Quaternion.Slerp(transform.rotation,
+    //                                                  Quaternion.LookRotation(direction),
+    //                                                  SchoolManager.SM.rotationSpeed * Time.deltaTime);
+    //    }
+    //}
 }
 
--- Assets/Scripts/SchoolManager.cs
+++ Assets/Scripts/SchoolManager.cs
@@ -7,16 +7,10 @@ public class SchoolManager : MonoBehaviour
 {
     public static SchoolManager SM;
     public GameObject fishPrefab;
-
-    public int schoolSize = 20;
+    public int numFish = 20;
     public GameObject[] allFish;
-
-    public Vector3 schoolLimit = new Vector3(10.0f, 10.0f, 10.0f);
-    public Vector3 goalPos = new Vector3(0, 0, 0);
-
-    public GameObject swimmingBounds;
-    private MeshRenderer swimmingBoundsMesh;
-
+    public Vector3 schoolLimits = new Vector3(5.0f, 5.0f, 5.0f);
+    public Vector3 goalPos = Vector3.zero;
 
     [Header("Fish Settings")]
     [Range(0.0f, 5.0f)] public float minSpeed;
@@ -24,42 +18,115 @@ public class SchoolManager : MonoBehaviour
     [Range(1.0f, 10.0f)] public float neighbourDistance;
     [Range(1.0f, 5.0f)] public float rotationSpeed;
 
-    [Header("Weighting Parameters")]
-    [Range(0.0f, 1.0f)] public float cohesionWeighting;
-    [Range(0.0f, 1.0f)] public float alignmentWeighting;
-    [Range(0.0f, 1.0f)] public float separationWeighting;
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        swimmingBoundsMesh = swimmingBounds.GetComponent<MeshRenderer>();
 
-        allFish = new GameObject[schoolSize];
-        for (int i = 0; i < schoolSize; ++i)
+        allFish = new GameObject[numFish];
+
+        for (int i = 0; i < numFish; ++i)
         {
+
             Vector3 pos = this.transform.position + new Vector3(
-                Random.Range(-schoolLimit.x, schoolLimit.x),
-                Random.Range(-schoolLimit.y, schoolLimit.y),
-                Random.Range(-schoolLimit.z, schoolLimit.z));
+                Random.Range(-schoolLimits.x, schoolLimits.x),
+                Random.Range(-schoolLimits.y, schoolLimits.y),
+                Random.Range(-schoolLimits.z, schoolLimits.z));
 
             allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
         }
 
         SM = this;
         goalPos = this.transform.position;
-
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Random.Range(0, 100) < 10)
-        {
-            goalPos = new Vector3(
-                Random.Range(-swimmingBoundsMesh.bounds.extents.x, swimmingBoundsMesh.bounds.extents.x),
-                Random.Range(-swimmingBoundsMesh.bounds.extents.y, swimmingBoundsMesh.bounds.extents.y),
-                Random.Range(-swimmingBoundsMesh.bounds.extents.z, swimmingBoundsMesh.bounds.extents.z));
-        }
+
+        //if (Random.Range(0, 100) < 10)
+        //{
+            //goalPos = this.transform.position;
+                //+ new Vector3(
+                //Random.Range(-schoolLimits.x, schoolLimits.x),
+                //Random.Range(-schoolLimits.y, schoolLimits.y),
+                //Random.Range(-schoolLimits.z, schoolLimits.z));
+        //}
     }
+    //public static SchoolManager SM;
+    //public GameObject fishPrefab;
+
+    //public int schoolSize = 20;
+    //public GameObject[] allFish;
+
+    //public Vector3 schoolLimits = new Vector3(10.0f, 10.0f, 10.0f);
+    //public Vector3 goalPos = new Vector3(0, 0, 0);
+
+    //public GameObject swimmingBounds;
+    //private MeshRenderer swimmingBoundsMesh;
+
+
+    //[Header("Fish Settings")]
+    //[Range(0.0f, 5.0f)] public float minSpeed;
+    //[Range(0.0f, 5.0f)] public float maxSpeed;
+    //[Range(1.0f, 10.0f)] public float neighbourDistance;
+    //[Range(1.0f, 5.0f)] public float rotationSpeed;
+
+    //[Header("Weighting Parameters")]
+    //[Range(0.0f, 10.0f)] public float cohesionWeighting;
+    //[Range(0.0f, 10.0f)] public float alignmentWeighting;
+    //[Range(0.0f, 10.0f)] public float separationWeighting;
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    swimmingBoundsMesh = swimmingBounds.GetComponent<MeshRenderer>();
+
+    //    allFish = new GameObject[schoolSize];
+    //    for (int i = 0; i < schoolSize; ++i)
+    //    {
+    //        Vector3 pos = this.transform.position + new Vector3(
+    //            Random.Range(-schoolLimits.x, schoolLimits.x),
+    //            Random.Range(-schoolLimits.y, schoolLimits.y),
+    //            Random.Range(-schoolLimits.z, schoolLimits.z));
+
+    //        allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+    //    }
+
+    //    SM = this;
+    //    goalPos = this.transform.position;
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Random.Range(0, 100) < 10)
+    //    {
+    //        //sets the bounds to generate the position within
+    //        Vector3 upperBound = swimmingBoundsMesh.bounds.max;
+    //        Vector3 lowerBound = swimmingBoundsMesh.bounds.min;
+
+    //        //generates a positon vector from the bounds provided
+    //        float xVal = Random.Range(upperBound.x, lowerBound.x);
+    //        float yVal = Random.Range(upperBound.y - 2, lowerBound.y);
+    //        float zVal = Random.Range(upperBound.z, lowerBound.z);
+
+    //        goalPos = new Vector3(xVal, yVal, zVal);
+
+    //        if (swimmingBoundsMesh.bounds.Contains(goalPos)) { Debug.Log("in bounds"); }
+    //        //float posXVal = swimmingBounds.transform.position.x + swimmingBoundsMesh.bounds.max.x;
+    //        //float negXVal = swimmingBounds.transform.position.x - swimmingBoundsMesh.bounds.max.x;
+
+    //        //float posYVal = swimmingBounds.transform.position.y + swimmingBoundsMesh.bounds.max.y;
+    //        //float negYVal = swimmingBounds.transform.position.y - swimmingBoundsMesh.bounds.max.y;
+
+    //        //float posZVal = swimmingBounds.transform.position.z + swimmingBoundsMesh.bounds.max.z;
+    //        //float negZVal = swimmingBounds.transform.position.z - swimmingBoundsMesh.bounds.max.z;
+
+    //        //goalPos = new Vector3(
+    //        //    Random.Range(negXVal, posXVal),
+    //        //    Random.Range(negYVal, posYVal),
+    //        //    Random.Range(negZVal, posYVal));
+    //    }
+    //}
 }
